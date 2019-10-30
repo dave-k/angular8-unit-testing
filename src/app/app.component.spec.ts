8e5dbd3813ca41e0a8a112c64ba643c57b03b709
David Kennedy <david@protogo.co>
2019-10-30 14:25:19 -0700

    add app.component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,22 +10,35 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  describe(':', () => {
+    // Begin by putting re-usable, preparatory code in a setup function instead of beforeEach().
+    // The setup() function returns an object literal with the variables, such as app, that a test might reference.
+    // You don't define semi-global variables (e.g., let app,fixture ) in the body of the describe().
+    // Then each test invokes setup() in its first line, before continuing with steps that
+    // manipulate the test subject and assert expectations.
 
-  it(`should have as title 'angular8-unit-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular8-unit-testing');
-  });
+    function setup() {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      return { fixture, app };
+    }
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8-unit-testing app is running!');
+    it('should create the app', () => {
+      const { app } = setup();
+      expect(app).toBeTruthy();
+    });
+
+    it(`should have as title 'angular8-unit-testing'`, () => {
+      const { app } = setup();
+      expect(app.title).toEqual('angular8-unit-testing');
+    });
+  
+    it('should render title', () => {
+      const { fixture } = setup();
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('.content span').textContent).toContain('angular8-unit-testing app is running!');
+    });
+  
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserAsyncComponent } from './user-async/user-async.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserAsyncComponent
   ],
   imports: [
     BrowserModule
